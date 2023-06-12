d697c24f6c90292c59e067f0c2e090e3cd51d29a
gguij004 <51983923+gguij004@users.noreply.github.com>
2023-06-12 09:10:43 -0400

    add unit testing
    initial setup for testing

---
+++ app/src/test/java/com/guijarro/smalldice/presenter/home_screen/HomeScreenViewModelTest.kt
@@ -0,0 +1,47 @@
+package com.guijarro.smalldice.presenter.home_screen
+
+import com.guijarro.smalldice.domain.GetNumberUseCase
+import io.mockk.mockk
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.TestDispatcher
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
+import org.junit.Assert.*
+
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.rules.TestWatcher
+import org.junit.runner.Description
+
+class HomeScreenViewModelTest {
+
+    @get:Rule
+    val mainDispatcherRule = MainDispatcherRule()
+
+    private lateinit var testObject: HomeScreenViewModel
+    private val mockGetNumberUseCase = mockk<GetNumberUseCase>()
+    private val testDispatcher = UnconfinedTestDispatcher()
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+}
+@OptIn(ExperimentalCoroutinesApi::class)
+class MainDispatcherRule(
+    private val testDispatcher: TestDispatcher = UnconfinedTestDispatcher()
+) : TestWatcher() {
+    override fun starting(description: Description) {
+        Dispatchers.setMain(testDispatcher)
+    }
+
+    override fun finished(description: Description) {
+        Dispatchers.resetMain()
+    }
+}
